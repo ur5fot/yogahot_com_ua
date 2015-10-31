0ac3ab25d8ea2596698f0bc0e03b6848f8a31358
ur5fot <wagvpered@gmail.com>
2015-10-31 13:09:35 +0200

    готово. мелкие ошибки при адоптивности по высоте

---
+++ js/main.js
@@ -0,0 +1,57 @@
+(function() {
+    'use strict';
+    var t, showTooltip;
+    var sunChildCenter = document.querySelector('.sun-child-center');
+    var sunChildCenterSpan = document.querySelector('.sun-child-center > span');
+
+    document.addEventListener('click', tooltipSunShow);
+
+    function tooltipSunShow(event) {
+        t = event.target;
+        if (t.hasAttribute('data-sun-tooltip')) {
+            t.classList.toggle('sun-child-hover');
+            if (showTooltip === t) {
+                sunChildCenter.classList.toggle('tooltipSun');
+
+            } else if (!sunChildCenter.classList.contains('tooltipSun')) {
+                sunChildCenter.classList.add('tooltipSun');
+            } else if (showTooltip !== t) {
+                showTooltip.classList.remove('sun-child-hover');
+            };
+            sunChildCenterSpan.innerHTML = t.getAttribute('data-sun-tooltip');
+            showTooltip = t;
+        } else {
+            return;
+        };
+    };
+
+
+    // if (sunH > h) {
+    //     sun.style.height = h + 'px';
+    //     sun.style.width =  'auto';
+    // };
+
+
+    var heightCash;
+    setInterval(function() {
+        var sunGlobal = document.querySelector('.sun-global');
+        var sun = document.querySelector('.sun  img');
+        var sunH = Math.round(sun.offsetHeight);
+        var sunGlobalH = Math.round(sunGlobal.offsetHeight);
+        var h = Math.round(document.documentElement.clientHeight);
+        var n;
+        if ( h < 611 && heightCash != h) {
+           n = sunH / sunGlobalH;
+            sun.style.height = Math.round(h * n) + 'px';
+            sun.style.width = 'auto';
+        } else if(612 <= h){
+            sun.style.height = '';
+            sun.style.width = '';
+        } ;
+
+        heightCash = h;
+
+    }, 1500);
+
+
+})();
